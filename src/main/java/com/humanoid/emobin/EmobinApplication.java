839522c600e06bb43dac02aae02daf110f999389
zldzldzz <wjl100735@gmail.com>
2025-06-30 21:47:31 +0900

    feat:DB추가 yml파일 형식으로 변경
    h2데ã이터 베이스 추가 및 yml형식으로 변경

--- src/main/java/com/humanoid/emobin/EmobinApplication.java
+++ src/main/java/com/humanoid/emobin/EmobinApplication.java
@@ -1,5 +1,6 @@
 package com.humanoid.emobin;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmobinApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		System.setProperty("H2_DB_PATH", dotenv.get("H2_DB_PATH"));
+
 		SpringApplication.run(EmobinApplication.class, args);
 	}
 
