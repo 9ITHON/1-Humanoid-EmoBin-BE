8891c8bf824a960db454ed56ba250c1fe5fc8bc1
Jaejung Park <jjp910123@gmail.com>
2025-07-11 23:54:00 +0900

    chore: 인가코드 확인용 로그 추가

--- src/main/java/com/humanoid/emobin/auth/controller/KakaoAuthController.java
+++ src/main/java/com/humanoid/emobin/auth/controller/KakaoAuthController.java
@@ -4,18 +4,21 @@ import com.humanoid.emobin.auth.AuthService;
 import com.humanoid.emobin.auth.dto.KakaoLoginRequest;
 import com.humanoid.emobin.auth.dto.LoginResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth/kakao")
+@Slf4j
 public class KakaoAuthController {
 
     private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody KakaoLoginRequest request) {
+        log.info("===={}====",request.getCode());
         LoginResponse loginResponse = authService.authenticateKakaoUser(request.getCode());
         return ResponseEntity.status(200).body(loginResponse);
     }
